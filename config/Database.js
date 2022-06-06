8903e7969893bd2a52073e7eb7efc9cab26c84b6
c7224x2043 <c7224x2043@bangkit.academy>
2022-06-06 22:58:25 +0700

    change-databasename

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('db_tes','root','luwakwhitecoffee',{
-    host: "34.101.250.155",
+const db = new Sequelize('dalangapp','root','tundalapardiperut',{
+    host: "34.101.61.109",
     dialect: "mysql"
 });
 
