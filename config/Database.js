e9316cdc488e05c8d13a2bc843693232c37a39e4
c7224x2043 <c7224x2043@bangkit.academy>
2022-06-06 22:42:09 +0700

    fix_Database

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('db_tes','root','',{
-    host: "localhost",
+const db = new Sequelize('db_tes','root','luwakwhitecoffee',{
+    host: "34.101.250.155",
     dialect: "mysql"
 });
 
