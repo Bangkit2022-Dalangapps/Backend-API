77c4b214f8254ccc684fc06e9b375092bfc97457
c7224x2043 <c7224x2043@bangkit.academy>
2022-06-07 19:59:44 +0700

    update

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,6 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('dalang-apps','root','tundalapardiperut',{
+const db = new Sequelize('dalangapp','root','tundalapardiperut',{
     host: "34.101.61.109",
     dialect: "mysql"
 });
